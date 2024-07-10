7f5886096895ecd489f4cbcf693822ed8e61a814
trasng <trangvan084@gmail.com>
2024-07-10 09:56:56 +0700

    trasng

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('ho');
-            $table->string('ten');
-            $table->date('ngay_sinh');
-            $table->string('sdt', 10)->unique();
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
