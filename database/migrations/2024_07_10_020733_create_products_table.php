a5f07f3d4999609bff388f4efbb76ba8a158b285
trasng <trangvan084@gmail.com>
2024-07-11 09:16:13 +0700

    dbase3

---
+++ database/migrations/2024_07_10_020733_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('sku')->unique();
+            $table->decimal('price', 10, 2);
+            $table->date('expiry_date');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->text('description');
+            $table->string('short_description');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
