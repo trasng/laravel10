f4d7a8a8477b00d3232df266f60a08b6dc826932
trasng <trangvan084@gmail.com>
2024-07-11 08:41:08 +0700

    dbase2

--- database/migrations/2024_07_10_020235_create_sub_categories_table.php
+++ database/migrations/2024_07_10_020235_create_sub_categories_table.php
@@ -11,16 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name');
             $table->string('name');
-            $table->date('birth_date');
-            $table->string('phone', 10)->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sub_categories');
     }
 };
